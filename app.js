740f77c7efccbd500f52fb04565b7caef886596f
carmenjoekalda <cjoekalda@gmail.com>
2024-04-01 21:15:18 +0300

    changed db structure, added sign in and sign up possibilities, port, sign up html form, updated mysql to mysql2

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
-const mysql = require('mysql')
-const port = 3020
+const mysql = require('mysql2/promise')
+const port = 3030
 const path = require('path');
 
 app.use(express.json());
@@ -23,20 +23,36 @@ app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
   });
 
-// Form's action
-app.post('/', (req, res) => {
-    const { name } = req.body;
+// sign up
+app.post('/signup', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const [result] = await pool.execute('INSERT INTO user (username, password) VALUES (?, ?)', [username, password]);
     
-    pool.query('INSERT INTO user (nimi) VALUES (?)', [name], (err, results) => {
-      if (err) {
-        console.error(err);
-        res.status(500).send('Error while adding name');
-      } else {
-        // Response
-        res.send('Name added successfully!');
-      }
-    });
-  });
+    res.status(201).json({ message: "Successfully signed up", userId: result.insertId });
+  } catch (error) {
+    if (error.code === 'ER_DUP_ENTRY') {
+      return res.status(400).json({ message: 'Username taken'});
+    }
+    res.status(500).json({ message: error.message });
+  } 
+});
+
+// sign in
+app.post('/signin', async (req, res) => {
+  try {
+    const { username, password } = req.body;
+    const [user] = await pool.execute('SELECT FROM user WHERE username = ?', [username]);
+    
+    if (user.length === 0 || user[0].password !== password) {
+      return res.status(401).json({ message: 'Sign in failed'});
+    }
+
+    res.status(200).json({ message: "Successfully signed in" });
+  } catch (error) {
+    res.status(500).json({ message: error.message});
+  } 
+});
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
