ca673e3b67cbd971b5f71794fe1fd32527b609e3
carmenjoekalda <cjoekalda@gmail.com>
2024-04-02 08:41:32 +0300

    iluvead, id removed

--- app.js
+++ app.js
@@ -29,8 +29,7 @@ app.post('/signup', async (req, res) => {
   try {
     const { username, password } = req.body;
     const [result] = await pool.execute('INSERT INTO user (username, password) VALUES (?, ?)', [username, password]);
-    
-    res.status(201).json({ message: "Successfully signed up", userId: result.insertId });
+    res.status(201).json({ message: "Successfully signed up" });
   } catch (error) {
     if (error.code === 'ER_DUP_ENTRY') {
       return res.status(400).json({ message: 'Username taken'});
@@ -44,11 +43,9 @@ app.post('/signin', async (req, res) => {
   try {
     const { username, password } = req.body;
     const [user] = await pool.execute('SELECT * FROM user WHERE username = ?', [username]);
-    
     if (user.length === 0 || user[0].password !== password) {
       return res.status(401).json({ message: 'Sign in failed'});
     }
-
     res.status(200).json({ message: "Successfully signed in" });
   } catch (error) {
     res.status(500).json({ message: error.message});
